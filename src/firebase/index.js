ae242185d96802fa924606f6f03a656a5621a47e
Simo Aleksandrov <simo3003@me.com>
2019-06-11 23:38:28 +0300

    Update antd

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,15 +1,15 @@
-import app from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firebase-firestore';
+import app from "firebase/app";
+import "firebase/auth";
+import "firebase/firebase-firestore";
 
 const config = {
-  apiKey: 'AIzaSyDeIjZqQ26u9zcLP0RFWjrLsSF85Q9JDoA',
-  authDomain: 'frontend-template-test.firebaseapp.com',
-  databaseURL: 'https://frontend-template-test.firebaseio.com',
-  projectId: 'frontend-template-test',
-  storageBucket: 'frontend-template-test.appspot.com',
-  messagingSenderId: '59991013837',
-  appId: '1:59991013837:web:3fcfd25df1a9f5ac'
+  apiKey: "AIzaSyDeIjZqQ26u9zcLP0RFWjrLsSF85Q9JDoA",
+  authDomain: "frontend-template-test.firebaseapp.com",
+  databaseURL: "https://frontend-template-test.firebaseio.com",
+  projectId: "frontend-template-test",
+  storageBucket: "frontend-template-test.appspot.com",
+  messagingSenderId: "59991013837",
+  appId: "1:59991013837:web:3fcfd25df1a9f5ac"
 };
 
 class Firebase {
